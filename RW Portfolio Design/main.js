bb8ae002de4805bc3743a0cc0f7cf33ca4794715
kanogithub <linkano.teck@gmail.com>
2022-07-07 23:56:54 +1000

    skill & work section

--- RW Portfolio Design/main.js
+++ RW Portfolio Design/main.js
@@ -6,3 +6,50 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+// Services Modal
+const modalViews = document.querySelectorAll('.services__modal'),
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mBtn, i) => {
+	mBtn.addEventListener('click', () => {
+		modal(i)
+	})
+})
+
+modalClose.forEach((mClose) => {
+	mClose.addEventListener('click', () => {
+		const tModal = document.querySelector('.services__modal.active-modal')
+		tModal.classList.remove('active-modal')
+	})
+})
+
+// Mixed up filter
+let mixerPortfolio = mixitup('.work__container', {
+	selectors: {
+		target: '.work__card',
+	},
+	animation: {
+		duration: 300,
+	},
+})
+
+const workBtns = document.querySelectorAll('.work__item')
+// const activeBtn = (e) => {
+// 	const deactiveChildren = Array.from(e.target.parentNode.children)
+// 	deactiveChildren.forEach((child) => child.classList.remove('active-work'))
+// 	e.target.classList.add('active-work')
+// }
+const activeWork = (e) => {
+	workBtns.forEach((btn) => btn.classList.remove('active-work'))
+	e.target.classList.add('active-work')
+}
+
+workBtns.forEach((btn) => {
+	btn.addEventListener('click', activeWork)
+})
