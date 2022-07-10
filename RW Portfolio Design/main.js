015d8c8d2c211296016114841d0a8d2f4d08c7bb
kanogithub <linkano.teck@gmail.com>
2022-07-10 23:39:57 +1000

    theme changes

--- RW Portfolio Design/main.js
+++ RW Portfolio Design/main.js
@@ -107,22 +107,22 @@ const iconTheme = 'bx-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? 'dark' : 'light')
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? 'light' : 'dark')
 const getCurrentIcon = () =>
-	themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+	themeButton.classList.contains(iconTheme) ? 'bx bx-sun' : 'bx bx-moon'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
-	themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+	document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](lightTheme)
+	themeButton.classList[selectedIcon === 'bx bx-sun' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-	// Add or remove the dark / icon theme
-	document.body.classList.toggle(darkTheme)
+	// Add or remove the light / icon theme
+	document.body.classList.toggle(lightTheme)
 	themeButton.classList.toggle(iconTheme)
 	// We save the theme and the current icon that the user chose
 	localStorage.setItem('selected-theme', getCurrentTheme())
