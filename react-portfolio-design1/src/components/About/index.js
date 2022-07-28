5b10c49423b417e8e1fff539b310a1a6236cb1d3
kanogithub <linkano.teck@gmail.com>
2022-07-28 16:59:43 +1000

    page transition spinner

--- react-portfolio-design1/src/components/About/index.js
+++ react-portfolio-design1/src/components/About/index.js
@@ -8,6 +8,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
@@ -21,54 +22,57 @@ function About() {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray="About me"
-            idx={15}
-          />
-        </h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
-          laudantium ab ea delectus aliquam quae voluptates animi maxime
-          exercitationem repellendus?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea corrupti
-          quod et neque impedit hic repudiandae quibusdam, id quidem aut
-          expedita voluptate ex quia maxime iste quas nobis placeat amet?
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde saepe
-          doloribus totam cumque repellendus excepturi!
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray="About me"
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            laudantium ab ea delectus aliquam quae voluptates animi maxime
+            exercitationem repellendus?
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea
+            corrupti quod et neque impedit hic repudiandae quibusdam, id quidem
+            aut expedita voluptate ex quia maxime iste quas nobis placeat amet?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde saepe
+            doloribus totam cumque repellendus excepturi!
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D2A" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D2A" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
